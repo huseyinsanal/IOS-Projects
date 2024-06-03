8c6a300cba7fb1e60e0dc4d2d56c159fd6e5f88a
Hüseyin Şanal <huseyinnsanal@gmail.com>
2024-06-03 22:43:26 +0300

    New commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+//String
+var myName = "Hüseyin"
+
+var mySurname = "Şanal"
+
+print(myName)
+
+myName = "hüseyin"
+
+print(myName)
+
+var userName = "test"
+userName.uppercased()
+userName.append("1")
+userName.count
+userName
+
+//Integer
+
+let myNumber = 10
+
+var myOtherNumber = 20
+myOtherNumber = 30
+myOtherNumber = 3
+
+let firstNumber = 20
+let secondNumber = 6
+
+firstNumber / secondNumber
+
+//Boolean
+
+var myBoolean = true
+
+myBoolean = false
+
+myNumber.isMultiple(of: 2)
+
+myNumber.isMultiple(of: 3)
+
+myNumber.isMultiple(of: 4)
+
+print(myNumber)
+
+//Double
+let pi = 3.14
+
+let doubleNumber = 80.0
+let secondDoubleNumber = 12.0
+
+doubleNumber / secondDoubleNumber
+
+
+var myThirdNumber : Int32 = 50
+myOtherNumber = 50000000000
+
+let myNewNumber : Float = 3.14
+
